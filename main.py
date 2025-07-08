46b70c3c707f2e297c4144d42b47f874c178185c
LilSarkay <120374746+LilSarkay@users.noreply.github.com>
2025-07-08 18:19:26 +0530

    Deploy: Hello App

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
-def main():
-    st.title('Greeting App')
-    name = st.text_input('Enter your name:')
-    if name:
-        st.write(f'Hello, {name}! Welcome to the Greeting App.')
-
-if __name__ == '__main__':
-    main()
+st.title('Hello, world!')
