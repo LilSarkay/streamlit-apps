0faa86c9b6ba88f577a33c65300eea28f9abe9ff
LilSarkay <120374746+LilSarkay@users.noreply.github.com>
2025-07-09 11:48:50 +0530

    Deploy: Simple Calculator

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import streamlit as st
 
-# Set up the app title
-st.title('Favorite Color App')
+st.title('Simple Calculator')
 
-# Create a dropdown to select the favorite color
-color = st.selectbox('Select your favorite color:', ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange'])
+# Getting user input for numbers
+number1 = st.number_input('Enter first number', value=0)
+number2 = st.number_input('Enter second number', value=0)
 
-# Display the selected favorite color
-st.write(f'Your favorite color is {color}.')
+# Perform calculations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+try:
+    division = number1 / number2
+except ZeroDivisionError:
+    division = 'Infinity'
+
+# Display results
+st.write(f"Addition: {addition}")
+st.write(f"Subtraction: {subtraction}")
+st.write(f"Multiplication: {multiplication}")
+st.write(f"Division: {division}")
