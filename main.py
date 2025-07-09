7819ecd3e36e9db5cf853d0829f98b59c9bf0a9c
LilSarkay <120374746+LilSarkay@users.noreply.github.com>
2025-07-09 16:09:42 +0530

    Deploy: Daily Planner v1.0

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-__updated__ = "Wed Jul  9 10:29:17 UTC 2025"
 import streamlit as st
 
-st.title('Simple Calculator')
+def main():
+    st.title('Daily Planner')
+    st.write("Organize your tasks efficiently.")
 
-# Input fields for numbers
-number1 = st.number_input('Enter first number', value=0.0)
-number2 = st.number_input('Enter second number', value=0.0)
-
-# Dropdown for selecting arithmetic operation
-operation = st.selectbox('Select Operation', ('Add', 'Subtract', 'Multiply', 'Divide'))
-
-# Calculate result based on the selected operation
-def calculate(num1, num2, op):
-    try:
-        if op == 'Add':
-            return num1 + num2
-        elif op == 'Subtract':
-            return num1 - num2
-        elif op == 'Multiply':
-            return num1 * num2
-        elif op == 'Divide':
-            if num2 == 0:
-                return 'Error: Division by zero'
-            else:
-                return num1 / num2
-    except Exception as e:
-        return f'Error: {str(e)}'
-
-# Button to perform calculation
-if st.button('Calculate'):
-    result = calculate(number1, number2, operation)
-    st.write('Result:', result)
+if __name__ == '__main__':
+    main()
