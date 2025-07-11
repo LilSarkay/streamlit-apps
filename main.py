f7013486ec42bcc4c9c17ca05797f8937efb954f
LilSarkay <120374746+LilSarkay@users.noreply.github.com>
2025-07-11 16:22:27 +0530

    Deploy: Unit Converter

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-__updated__ = "Fri Jul 11 10:45:10 UTC 2025"
 import streamlit as st
 
-def calculate(num1, num2, operation):
-    if operation == 'Add':
-        return num1 + num2
-    elif operation == 'Subtract':
-        return num1 - num2
-    elif operation == 'Multiply':
-        return num1 * num2
-    elif operation == 'Divide':
-        try:
-            return num1 / num2
-        except ZeroDivisionError:
-            return 'Error: Division by zero'
+def convert_units(value, from_unit, to_unit):
+    conversion_factors = {
+        'meters': 1,
+        'kilometers': 0.001,
+        'miles': 0.000621371,
+        'feet': 3.28084
+    }
+    if from_unit not in conversion_factors or to_unit not in conversion_factors:
+        return null
+    value_in_meters = value / conversion_factors[from_unit]
+    return value_in_meters * conversion_factors[to_unit]
 
-st.title('Basic Calculator')
+st.title('Unit Converter')
 
-num1 = st.number_input('Enter the first number', format='%f')
-num2 = st.number_input('Enter the second number', format='%f')
+value = st.number_input('Enter the value to be converted', min_value=0.0)
+from_unit = st.selectbox('From Unit', ['meters', 'kilometers', 'miles', 'feet'])
+to_unit = st.selectbox('To Unit', ['meters', 'kilometers', 'miles', 'feet'])
 
-operation = st.selectbox('Select operation', ('Add', 'Subtract', 'Multiply', 'Divide'))
-
-if st.button('Calculate'):
-    result = calculate(num1, num2, operation)
-    st.write(f'The result is: {result}')
+if st.button('Convert'):
+    result = convert_units(value, from_unit, to_unit)
+    if result is not null:
+        st.write(f'{value} {from_unit} is equal to {result} {to_unit}')
+    else:
+        st.write('Invalid unit conversion')
