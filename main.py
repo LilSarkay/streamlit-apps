64a1c2364456ee130a88e64f95702b8cdddf8863
LilSarkay <120374746+LilSarkay@users.noreply.github.com>
2025-07-11 16:40:53 +0530

    Deploy: Unit Converter

--- main.py
+++ main.py
@@ -1,107 +1,75 @@
-__updated__ = "Fri Jul 11 11:09:57 UTC 2025"
 import streamlit as st
 
 # Title of the app
-st.title('Unit Converter')
+st.title("Unit Converter")
 
-# Select the type of conversion
-type_of_conversion = st.selectbox('Select the type of conversion you would like to perform:', ['Length', 'Temperature', 'Weight'])
+# Sidebar for selecting conversion type
+conversion_type = st.sidebar.selectbox("Select conversion type", ('Length', 'Weight', 'Temperature'))
 
-if type_of_conversion == 'Length':
-    # Length conversion
-    units = ['Meters', 'Kilometers', 'Miles', 'Feet']
-    input_unit = st.selectbox('Choose unit to convert from:', units)
-    output_unit = st.selectbox('Choose unit to convert to:', units)
-    value = st.number_input(f'Enter value in {input_unit}:', min_value=0.0)
-    
-    # Conversion logic
-    if input_unit == 'Meters' and output_unit == 'Kilometers':
-        converted_value = value / 1000
-    elif input_unit == 'Kilometers' and output_unit == 'Meters':
-        converted_value = value * 1000
-    elif input_unit == 'Meters' and output_unit == 'Miles':
-        converted_value = value / 1609.34
-    elif input_unit == 'Miles' and output_unit == 'Meters':
-        converted_value = value * 1609.34
-    elif input_unit == 'Kilometers' and output_unit == 'Miles':
-        converted_value = value / 1.60934
-    elif input_unit == 'Miles' and output_unit == 'Kilometers':
-        converted_value = value * 1.60934
-    elif input_unit == 'Feet' and output_unit == 'Meters':
-        converted_value = value / 3.28084
-    elif input_unit == 'Meters' and output_unit == 'Feet':
-        converted_value = value * 3.28084
-    elif input_unit == 'Feet' and output_unit == 'Miles':
-        converted_value = value / 5280
-    elif input_unit == 'Miles' and output_unit == 'Feet':
-        converted_value = value * 5280
-    elif input_unit == 'Feet' and output_unit == 'Kilometers':
-        converted_value = value / 3280.84
-    elif input_unit == 'Kilometers' and output_unit == 'Feet':
-        converted_value = value * 3280.84
-    else:
-        converted_value = value  # Same unit
-    
-    st.write(f'{value} {input_unit} is equal to {converted_value} {output_unit}')
-    
-elif type_of_conversion == 'Temperature':
-    # Temperature conversion
-    units = ['Celsius', 'Fahrenheit', 'Kelvin']
-    input_unit = st.selectbox('Choose unit to convert from:', units)
-    output_unit = st.selectbox('Choose unit to convert to:', units)
-    value = st.number_input(f'Enter temperature in {input_unit}:', min_value=-273.15)
-    
-    # Conversion logic
-    if input_unit == 'Celsius' and output_unit == 'Fahrenheit':
-        converted_value = (value * 9/5) + 32
-    elif input_unit == 'Fahrenheit' and output_unit == 'Celsius':
-        converted_value = (value - 32) * 5/9
-    elif input_unit == 'Celsius' and output_unit == 'Kelvin':
-        converted_value = value + 273.15
-    elif input_unit == 'Kelvin' and output_unit == 'Celsius':
-        converted_value = value - 273.15
-    elif input_unit == 'Fahrenheit' and output_unit == 'Kelvin':
-        converted_value = (value + 459.67) * 5/9
-    elif input_unit == 'Kelvin' and output_unit == 'Fahrenheit':
-        converted_value = (value * 9/5) - 459.67
-    else:
-        converted_value = value  # Same unit
-    
-    st.write(f'{value} {input_unit} is equal to {converted_value} {output_unit}')
+# Function to convert length
+def convert_length(value, from_unit, to_unit):
+    conversion_factors = {
+        'meters': 1.0,
+        'kilometers': 0.001,
+        'centimeters': 100.0,
+        'millimeters': 1000.0,
+        'miles': 0.000621371,
+        'yards': 1.09361,
+        'feet': 3.28084,
+        'inches': 39.3701
+    }
+    return value * conversion_factors[to_unit] / conversion_factors[from_unit]
 
-elif type_of_conversion == 'Weight':
-    # Weight conversion
-    units = ['Grams', 'Kilograms', 'Pounds', 'Ounces']
-    input_unit = st.selectbox('Choose unit to convert from:', units)
-    output_unit = st.selectbox('Choose unit to convert to:', units)
-    value = st.number_input(f'Enter weight in {input_unit}:', min_value=0.0)
-    
-    # Conversion logic
-    if input_unit == 'Grams' and output_unit == 'Kilograms':
-        converted_value = value / 1000
-    elif input_unit == 'Kilograms' and output_unit == 'Grams':
-        converted_value = value * 1000
-    elif input_unit == 'Grams' and output_unit == 'Pounds':
-        converted_value = value / 453.592
-    elif input_unit == 'Pounds' and output_unit == 'Grams':
-        converted_value = value * 453.592
-    elif input_unit == 'Kilograms' and output_unit == 'Pounds':
-        converted_value = value * 2.20462
-    elif input_unit == 'Pounds' and output_unit == 'Kilograms':
-        converted_value = value / 2.20462
-    elif input_unit == 'Ounces' and output_unit == 'Grams':
-        converted_value = value * 28.3495
-    elif input_unit == 'Grams' and output_unit == 'Ounces':
-        converted_value = value / 28.3495
-    elif input_unit == 'Pounds' and output_unit == 'Ounces':
-        converted_value = value * 16
-    elif input_unit == 'Ounces' and output_unit == 'Pounds':
-        converted_value = value / 16
-    elif input_unit == 'Kilograms' and output_unit == 'Ounces':
-        converted_value = value * 35.274
-    elif input_unit == 'Ounces' and output_unit == 'Kilograms':
-        converted_value = value / 35.274
+# Function to convert weight
+def convert_weight(value, from_unit, to_unit):
+    conversion_factors = {
+        'kilograms': 1.0,
+        'grams': 1000.0,
+        'pounds': 2.20462,
+        'ounces': 35.274
+    }
+    return value * conversion_factors[to_unit] / conversion_factors[from_unit]
+
+# Function to convert temperature
+def convert_temperature(value, from_unit, to_unit):
+    if from_unit == 'celsius' and to_unit == 'fahrenheit':
+        return (value * 9/5) + 32
+    elif from_unit == 'fahrenheit' and to_unit == 'celsius':
+        return (value - 32) * 5/9
+    elif from_unit == 'celsius' and to_unit == 'kelvin':
+        return value + 273.15
+    elif from_unit == 'kelvin' and to_unit == 'celsius':
+        return value - 273.15
+    elif from_unit == 'fahrenheit' and to_unit == 'kelvin':
+        return (value - 32) * 5/9 + 273.15
+    elif from_unit == 'kelvin' and to_unit == 'fahrenheit':
+        return (value - 273.15) * 9/5 + 32
     else:
-        converted_value = value  # Same unit
-    
-    st.write(f'{value} {input_unit} is equal to {converted_value} {output_unit}')
+        return value
+
+if conversion_type == 'Length':
+    st.header("Length Converter")
+    length_units = ['meters', 'kilometers', 'centimeters', 'millimeters', 'miles', 'yards', 'feet', 'inches']
+    value = st.number_input("Enter length", min_value=0.0)
+    from_unit = st.selectbox("From", length_units)
+    to_unit = st.selectbox("To", length_units)
+    result = convert_length(value, from_unit, to_unit)
+    st.write("{} {} is {} {}".format(value, from_unit, result, to_unit))
+
+elif conversion_type == 'Weight':
+    st.header("Weight Converter")
+    weight_units = ['kilograms', 'grams', 'pounds', 'ounces']
+    value = st.number_input("Enter weight", min_value=0.0)
+    from_unit = st.selectbox("From", weight_units)
+    to_unit = st.selectbox("To", weight_units)
+    result = convert_weight(value, from_unit, to_unit)
+    st.write("{} {} is {} {}".format(value, from_unit, result, to_unit))
+
+elif conversion_type == 'Temperature':
+    st.header("Temperature Converter")
+    temperature_units = ['celsius', 'fahrenheit', 'kelvin']
+    value = st.number_input("Enter temperature")
+    from_unit = st.selectbox("From", temperature_units)
+    to_unit = st.selectbox("To", temperature_units)
+    result = convert_temperature(value, from_unit, to_unit)
+    st.write("{} {} is {} {}".format(value, from_unit, result, to_unit))
